c51a09077ea8a41af30ce1da6767e4fc71e73d09
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-02-17 15:15:04 +0400

    Factorial

--- Calculator.java
+++ Calculator.java
@@ -23,6 +23,12 @@ public class Calculator {
         double power = Math.pow(num1, num2);        
         System.out.println("power: "+ power);
 
+        System.out.println("*** Factorial of First Number ***");
+        int result = 1; // Start From 1; because if we start from 0 the result always 0
+        for (int i = 1; i <= num1; i++) {
+            result = result * i;
+        }
+        System.out.println("Factorial of " + num1 + " is: " + result);
         scanner.close();
     }
 }
