b44d6581c73e77d0e24a21e4cddd3f3b25ac301e
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-02-17 10:32:51 +0400

    minar changes

--- Calculator.java
+++ Calculator.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter first number:");
+        System.out.print("Enter first number: ");
         int num1 = scanner.nextInt();
-        System.out.println("Enter second number:");
+        System.out.print("Enter second number: ");
         int num2 = scanner.nextInt();
 
         // Multiplication and Division
